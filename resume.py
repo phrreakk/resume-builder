37ee0df767fa379f6456f6626f1d8faadd28bdcd
Kevin Gangi <kevin@gangi-home.us>
2025-02-03 11:35:21 -0500

    Added sections for certs and honors

--- resume.py
+++ resume.py
@@ -24,6 +24,8 @@ with open(resume_yaml, 'r') as file:
     intro = resume["intro"]
     experience = resume["experience"]
     education = resume["education"]
+    certifications = resume["certifications"]
+    honors = resume["honors"]
 
     html = template.render(name = name,
                            title = title,
@@ -31,7 +33,9 @@ with open(resume_yaml, 'r') as file:
                            skills = skills,
                            intro = intro,
                            experience = experience,
-                           education = education)
+                           education = education,
+                           certifications = certifications,
+                           honors = honors)
 
     options = {
       "enable-local-file-access": None,
